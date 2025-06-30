4993c3bb2a266f2ac7e10dee9d10f9f0c8ddbd83
imane sebbar <168044281+imanesebb@users.noreply.github.com>
2025-06-30 20:04:50 +0100

    Update db_connection.py

--- APP/db_connection.py
+++ APP/db_connection.py
@@ -3,7 +3,7 @@ import oracledb
 def get_connection():
     return oracledb.connect(
                  user="SYS",
-                 password="niggaballz12345678",
-                 dsn="localhost/orcliman",
+                 password="",
+                 dsn="",
                  mode=oracledb.SYSDBA
     )
